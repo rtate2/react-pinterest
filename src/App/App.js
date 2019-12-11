d6d346ec536d7dadeca0b38b2ff5f3d16ab4da40
Randy Tate <randytatejr@gmail.com>
2019-12-10 21:34:31 -0600

    auth completed

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase/app';
+
+import firebaseConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/auth';
+
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-danger">More Stuff</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebaseConnection.firebaseApp();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+          <button className="btn btn-danger">Bootstrap Button</button>
+          {/* If they are authenticated, load the board */}
+          {/* ?\else show login button */}
+          {
+            (authed) ? (<div>You logged in</div>) : (<Auth />)
+          }
+      </div>
+    );
+  }
 }
 
 export default App;
